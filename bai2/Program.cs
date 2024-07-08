9a19cf821463b0154f766072088a7b16271c82f4
Administrator <Administrator@LAB1-MAY34>
2024-07-08 16:02:27 +0700

    Add project files.

---
+++ bai2/Program.cs
@@ -0,0 +1,52 @@
+﻿using bai2;
+List<phimvideo> movielist = new List<phimvideo>();
+while (true)
+{
+    Console.WriteLine("\n===== Quan ly phim VIDEO =====");
+    Console.WriteLine("phimmoi. Ten bo phim moi");
+    Console.WriteLine("danh sach. Hien thi danh sach ");
+    Console.WriteLine("next.thoat");
+    Console.Write("Vui long chon phimmoi/danh/next");
+    string choice = Console.ReadLine(); 
+    switch (choice)
+    {
+        case "phimmoi":
+            Console.WriteLine("\nNhap thong tin cho bo phim moi:");
+            Console.Write ("tTen bo phim: ");
+            string ten = Console.ReadLine();
+            Console.Write("The loai");
+            string theloai = Console.ReadLine();
+            Console.Write("tTen bo phim: ");
+            string Daovien = Console.ReadLine();
+            Console.Write("Dao vien");
+            string daovien = Console.ReadLine();
+            Console.Write("dvnam");
+            string dvnam = Console.ReadLine();
+            Console.Write("hang san xuat: ");
+            string hsx = Console.ReadLine();
+            Console.Write("Ten dvnu: ");
+            string dvnu = Console.ReadLine();
+            Console.Write("nam san xuat: ");
+            string nsx = Console.ReadLine();
+
+            phimvideo movie = new phimvideo(ten, theloai, daovien, dvnam, dvnu,nsx, hsx);
+            movielist.Add(movie);
+            Console.WriteLine("\n Da them bo phoim moi vao danh sach.");
+            break;
+        case "danh sach":
+            Console.WriteLine("\n danh sach cac bo phim:");
+            foreach (var mv in movielist)
+            {
+                mv.Hienthiphimvideo();
+            }
+            break;
+
+        case "next":
+            Console.WriteLine("\n Da thoat chuong trinh.");
+            return;
+         
+        default:
+            Console.WriteLine("\n Lua chon khong hop le. vui long chon lai.");
+            break;
+    }    
+}
---
+++ bai2/phimvideo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2
+{
+    internal class phimvideo
+    {
+        public string Tenphim { get; set; }
+        public string Daovien { get; set; }
+        public string Theloai { get; set; }
+        public int Nsxuat { get; set; }
+        public string Hsx { get; set; }
+        public string Tendvn { get; set; }
+        public string Tendvnam { get; set; }
+
+        public phimvideo(string theloai, string Nsxuat, string hsx,string daovien, string tenndvn, string tenndvnam, string tenphim)
+        {
+            Theloai = theloai;
+            Daovien = daovien;
+            Nsxuat = Nsxuat;
+            Hsx = hsx;
+            Tendvn = tenndvn;
+            Tendvnam = tenndvnam;
+            Tenphim = tenphim;
+        }
+        public void Hienthiphimvideo()
+        {
+            Console.WriteLine("Ten phimvideo: " + Tenphim);
+            Console.WriteLine("Ten theloai: " + Theloai);
+            Console.WriteLine("Ten Tendvn: " + Tendvn);
+            Console.WriteLine("Ten Tendvnam: " + Tendvnam);
+            Console.WriteLine("Nsxuat:" + Nsxuat);
+            Console.WriteLine("Hsx:" + Hsx);
+            Console.WriteLine();
+        }
+    }
+}
